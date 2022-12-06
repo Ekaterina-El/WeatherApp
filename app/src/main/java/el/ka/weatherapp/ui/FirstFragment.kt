c004fe0843366eb46b3aae5c2b27ffe0b7a46e4f
Ekaterina-El <hackerrussia@yandex.ru>
2022-12-06 14:38:18 +0500

    Created layout for first screen

---
+++ app/src/main/java/el/ka/weatherapp/ui/FirstFragment.kt
@@ -0,0 +1,7 @@
+package el.ka.weatherapp.ui
+
+import androidx.fragment.app.Fragment
+import el.ka.weatherapp.R
+
+class FirstFragment: Fragment(R.layout.first_fragment) {
+}
--- app/src/main/java/el/ka/weatherapp/ui/SecondFragment.kt
+++
@@ -1,6 +0,0 @@
-package el.ka.weatherapp.ui
-
-import androidx.fragment.app.Fragment
-
-class SecondFragment: Fragment {
-}
