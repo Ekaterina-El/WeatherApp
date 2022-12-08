678627a1aae8f297f0aec9e151140c6603efd21b
Ekaterina-El <hackerrussia@yandex.ru>
2022-12-08 20:01:30 +0500

    Show average temperature

--- app/src/main/java/el/ka/weatherapp/data/model/YearSeasonType.kt
+++ app/src/main/java/el/ka/weatherapp/data/model/YearSeasonType.kt
@@ -1,11 +1,10 @@
 package el.ka.weatherapp.data.model
 
-import el.ka.weatherapp.R
 
-enum class YearSeasonType {
-  SPRING,
-  SUMMER,
-  AUTUMN,
-  WINTER
+enum class YearSeasonType(val months: Array<Int>) {
+  SPRING(arrayOf(2, 3, 4)),
+  SUMMER(arrayOf(5, 6, 7)),
+  AUTUMN(arrayOf(8, 9, 10)),
+  WINTER(arrayOf(11, 0, 1))
 }
 
